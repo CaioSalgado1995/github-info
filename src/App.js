72cc60e17031205a69631d96f2c49bce5545d3a3
Caio Luiz Salgado <caio.salgado@ebanx.com>
2022-02-14 19:57:01 -0300

    Adding logic to get a list of repositories using axios http client

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react/cjs/react.production.min';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -7,7 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       githubUsername: "",
-      repoNumber: 0
+      repoError: false,
+      repoInfo: []
     };
   }
 
@@ -19,27 +21,68 @@ class App extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // TODO install axios
-    // TODO call github API and change the repoNumber state property  
+    const config = {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    };
+    axios.get(`https://api.github.com/users/${this.state.githubUsername}/repos`, config)
+      .then(res => {
+        this.setState({ 
+          repoError: false, 
+          repoInfo: res.data,
+          githubUsername: "" });
+      }).catch(_err => {
+        this.setState({ repoError: true, repoInfo: [], githubUsername: "" });
+      });
+  }
+
+  renderError() {
+    return (
+      <div>
+        <h2>This username doesn't have any repository</h2>
+      </div>
+    );
+  }
+
+  renderRepositoryList() {
+    return (
+      <ul>
+        <li className='repo-list-item'>
+          Repository fullname - Repository stars
+        </li>
+        {
+          this.state.repoInfo.map((item, index) => {
+            return (
+              <li className='repo-list-item' key={index}>
+                {item.full_name} - {item.stargazers_count}
+              </li>
+            )
+          })
+        }
+      </ul>
+    );
   }
 
   render() {
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <input 
+          <input
             type="text"
             placeholder="Type your github username"
+            value={this.state.githubUsername}
             onChange={this.handleUsernameChange.bind(this)} />
-          <input type="submit" value="Send"/>
+          <input type="submit" value="Send" />
         </form>
-        <div>
-          <h2>{this.state.repoNumber}</h2>
-        </div>
+        { this.state.repoError ? 
+          this.renderError() : 
+          this.renderRepositoryList()
+        }
       </div>
     );
   }
-  
+
 }
 
 export default App;
